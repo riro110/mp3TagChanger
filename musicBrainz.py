8e01f63d165dd4bd7c535c651621d139d7ff86de
lilo97 <urutoraman183@gmail.com>
2018-12-07 11:14:26 +0900

    fix musicBrainz.py

--- musicBrainz.py
+++ musicBrainz.py
@@ -1,17 +1,18 @@
-#MusicBrainz API のテスト
+# MusicBrainz API のテスト
 import requests
 import json
 
-url = "http://musicbrainz.org/ws/2/search?"
+url = "http://musicbrainz.org/ws/2/artist/3798b104-01cb-484c-a3b0-56adc6399b80"
 
 params = {
-    "query": "Rage+Against+The+Machine",
-    "type":"artist"
+    "fmt": "json"
 }
 
 
-res = requests.get(url,params=params)
+res = requests.get(url, params=params)
 
 if res.status_code == 200:
     with open("json/RATM_musicbrainz.json", "w") as fp:
-        json.dump(res.json(),fp,indent=2)
+        json.dump(res.json(), fp, indent=2)
+else:
+    print(res.status_code)
