65921d9ce5a923d81df67927634b1d8eade2a40e
lilo97 <urutoraman183@gmail.com>
2018-12-08 13:52:31 +0900

    json追加

--- musicBrainz.py
+++ musicBrainz.py
@@ -2,17 +2,20 @@
 import requests
 import json
 
-url = "http://musicbrainz.org/ws/2/artist/3798b104-01cb-484c-a3b0-56adc6399b80"
-
+#url = "http://musicbrainz.org/ws/2/release/05f8a9f4-2ed6-49b9-8b07-ea62879aa9b5"
+url = "http://musicbrainz.org/ws/2/release"
 params = {
-    "fmt": "json"
+    "fmt": "json",
+    "query": "zedd",
+    "limit": "1",
 }
 
 
 res = requests.get(url, params=params)
 
 if res.status_code == 200:
-    with open("json/RATM_musicbrainz.json", "w") as fp:
+    with open("json/ZEDD_musicbrainz.json", "w") as fp:
         json.dump(res.json(), fp, indent=2)
+    print("OK")
 else:
     print(res.status_code)
